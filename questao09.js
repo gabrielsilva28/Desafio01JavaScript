8524bc4665d2e1221e0bec922edb889360c41331
Gabriel Silva <87430469+gabrielsilva28@users.noreply.github.com>
2024-04-03 10:12:33 -0300

    Update questao09.js

--- questao09.js
+++ questao09.js
@@ -2,14 +2,18 @@ function contaVida(hp, ataque) {
   let dano = 20;
   let hpFinal = 0;
   for (let i = 0; i < hp; i++) {
-    if (ataque > 0) {
-      hpFinal = hp - dano * ataque;
+    if (ataque <= 5) {
+        hpFinal = hp - dano * ataque;
+    }
+    else{
+      hpFinal = 0;
     }
   }
   return hpFinal;
 }
 
 let vida = 100;
-let ataques = 4;
+let ataques = 3;
 let combate = contaVida(vida, ataques);
 console.log(`Após ${ataques} ataques restaram ${combate} de hp`);
+
